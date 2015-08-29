beba8b089b9c242156b3b0abc49fec385f2e5970
Nikolas Poniros <nikolas@poniros.de>
2015-08-29 17:50:02 +0200

    convert db_wrapper into a factory so we can use it with various stores

--- src/db/db_wrapper.js
+++ src/db/db_wrapper.js
@@ -1,19 +1,37 @@
 import * as IndexedDB from './indexeddb_connector.js';
 import getQueryFunction from './queries.js';
 
-const dbStoreName = 'Bookmarks';
-export function save(data) {
-  return IndexedDB.save(dbStoreName, data);
+const instances = new Map();
+
+class DBWrapper {
+  constructor(dbStoreName) {
+    this.dbStoreName = dbStoreName;
+  }
+
+  save(data) {
+    return IndexedDB.save(this.dbStoreName, data);
+  }
+
+  remove(id) {
+    return IndexedDB.remove(this.dbStoreName, id);
+  }
+
+  getAll(query) {
+    return IndexedDB.getAll(this.dbStoreName, getQueryFunction(query));
+  }
+
+  getOne(id) {
+    return IndexedDB.getOne(this.dbStoreName, id);
+  }
 }
 
-export function remove(id) {
-  return IndexedDB.remove(dbStoreName, id);
-}
-
-export function getAll(query) {
-  return IndexedDB.getAll(dbStoreName, getQueryFunction(query));
-}
-
-export function getOne(id) {
-  return IndexedDB.getOne(dbStoreName, id);
+export default function getInstance(dbStoreName) {
+  const instance = instances.get(dbStoreName);
+  if (instance) {
+    return instance;
+  } else {
+    const newInstance = new DBWrapper(dbStoreName);
+    instances.set(dbStoreName, newInstance);
+    return newInstance;
+  }
 }
