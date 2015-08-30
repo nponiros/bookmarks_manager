b7cf207236cf18fec407fceb525a4fdb79b9d53e
Nikolas Poniros <nikolas@poniros.de>
2015-08-30 17:11:28 +0200

    add validation for new tag

--- src/components/tags/add_new_tag.jsx
+++ src/components/tags/add_new_tag.jsx
@@ -5,18 +5,33 @@ import {Button, Input} from 'react-bootstrap';
 import {create} from '../../actions/tag_actions.js';
 
 class AddNewTag extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nameValidStyle: 'error'
+    };
+  }
+
   handleSubmit(event) {
     event.preventDefault();
-    const tag = {
-      name: this.refs.name.getValue(),
-      bookmarkIds: []
-    };
-    create(tag);
+    const nameInput = this.refs.name.getInputDOMNode();
+    const nameValid = nameInput.checkValidity();
+    if (nameValid) {
+      const tag = {
+        name: this.refs.name.getValue(),
+        bookmarkIds: []
+      };
+      create(tag);
+    } else {
+      this.setState({
+        nameValidStyle: nameValid ? undefined : 'error'
+      });
+    }
   }
 
   render() {
-    return <form onSubmit={(event) => this.handleSubmit(event)}>
-      <Input type="text" label="New tag" ref="name"/>
+    return <form onSubmit={(event) => this.handleSubmit(event)} noValidate>
+      <Input type="text" label="New tag" ref="name" required hasFeedback bsStyle={this.state.nameValidStyle}/>
       <Button type="submit" bsStyle="primary">Add Tag</Button>
     </form>;
   }
