9dad438c22f12abbb05b2d8a2922e705f6f5315e
Nikolas Poniros <nikolas@poniros.de>
2015-10-31 15:49:37 +0100

    update react and react-bootstrap

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import BmApp from './components/bm_app.js';
 
-React.render(
-  <BmApp/>,
+ReactDOM.render(
+  React.createElement(BmApp, null),
   document.getElementById('bm-app')
 );
+
