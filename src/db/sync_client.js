1da21153e5823b5ffbf872c33cf1e23448685f09
Nikolas Poniros <nikolas@poniros.de>
2015-12-31 18:11:19 +0100

    don't pass server url to sync client constructor

--- src/db/sync_client.js
+++ src/db/sync_client.js
@@ -1,7 +1,7 @@
 import SyncClient from 'syncClient';
 import {DB_STORE_NAME as BOOKMARKS_STORE} from '../constants/bookmarks_constants.js';
 import {DB_STORE_NAME as TAGS_STORE} from '../constants/tags_constants.js';
-import {SERVER_URL, DB_NAME} from '../constants/sync_constants.js';
+import {DB_NAME} from '../constants/sync_constants.js';
 
-const syncClient = new SyncClient(DB_NAME, [BOOKMARKS_STORE, TAGS_STORE], SERVER_URL);
+const syncClient = new SyncClient(DB_NAME, [BOOKMARKS_STORE, TAGS_STORE]);
 export default syncClient;
