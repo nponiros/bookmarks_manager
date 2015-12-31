42d40defba6f95fda469efbd181d955dd733c807
Nikolas Poniros <nikolas@poniros.de>
2015-12-31 13:12:48 +0100

    query a server to really check the online status

--- src/actions/connection_status_actions.js
+++ src/actions/connection_status_actions.js
@@ -1,26 +1,65 @@
 import AppDispatcher from '../dispatcher/app_dispatcher.js';
-import {ONLINE, OFFLINE} from '../constants/connection_status_constants.js';
+import {ONLINE, OFFLINE, CONNECTION_CHECK_PATH} from '../constants/connection_status_constants.js';
+import {SERVER_URL} from '../constants/sync_constants.js';
 
-function startStatusChecker() {
+function dispatch(actionType) {
+  AppDispatcher.dispatch({
+    actionType
+  });
+}
+
+function checkServerConnection() {
+  return window.fetch(`${SERVER_URL}${CONNECTION_CHECK_PATH}`, {
+    method: 'HEAD'
+  });
+}
+
+function statusChecker() {
+  let navigatorOnline = window.navigator.onLine;
+  const checkInterval = 30000;
+
+  // navigator.onLine === true is no guarantee that the server
+  // is also online. Check that before dispatching ONLINE
   function statusChangedToOnline() {
-    AppDispatcher.dispatch({
-      actionType: ONLINE
+    navigatorOnline = true;
+    checkServerConnection().then(() => {
+      dispatch(ONLINE);
+    }).catch(() => {
+      dispatch(OFFLINE);
     });
   }
 
   function statusChangedToOffline() {
-    AppDispatcher.dispatch({
-      actionType: OFFLINE
-    });
+    navigatorOnline = false;
+    dispatch(OFFLINE);
   }
 
   window.addEventListener('online', statusChangedToOnline);
   window.addEventListener('offline', statusChangedToOffline);
+
+  // check every now and then if the server is online
+  setInterval(() => {
+    if (navigatorOnline) {
+      checkServerConnection().then(() => {
+        dispatch(ONLINE);
+      }).catch(() => {
+        dispatch(OFFLINE);
+      });
+    }
+  }, checkInterval);
+
+  // Initial check
+  if (navigatorOnline) {
+    checkServerConnection().then(() => {
+      dispatch(ONLINE);
+    }).catch(() => {
+      dispatch(OFFLINE);
+    });
+  } else {
+    dispatch(OFFLINE);
+  }
 }
 
 export function init() {
-  startStatusChecker();
-  AppDispatcher.dispatch({
-    actionType: window.navigator.onLine ? ONLINE : OFFLINE
-  });
+  statusChecker();
 }
--- src/constants/connection_status_constants.js
+++ src/constants/connection_status_constants.js
@@ -1,4 +1,5 @@
 export const ONLINE = 'is_online';
 export const OFFLINE = 'is_offline';
+export const CONNECTION_CHECK_PATH = '/api/v1/check';
 
 export const CHANGE = 'connection_status_changed';
