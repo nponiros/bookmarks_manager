2a225d11072daf7190eb92bfbf45d1c4bc0340fb
Nikolas Poniros <nikolas@poniros.de>
2016-12-27 19:44:28 +0100

    sort items

--- app/src/containers/App/index.js
+++ app/src/containers/App/index.js
@@ -3,6 +3,32 @@ import { connect } from 'react-redux';
 import App from '../../components/App';
 
 import handleAction from '../../actions';
+import { FOLDER } from '../../constants';
+
+function sortItems(items, entities) {
+  const sortedItems = [...items];
+  sortedItems.sort(function(firstID, secondID) {
+    const firstEntity = entities[firstID];
+    const secondEntity = entities[secondID];
+
+    // if types match -> sort title
+    if (firstEntity.type === secondEntity.type) {
+      if (firstEntity.title < secondEntity.title) {
+        return -1;
+      } else if (firstEntity.title === secondEntity.title) {
+        return 0;
+      }
+      return 1;
+    }
+
+    // Folders come before Bookmarks
+    if (firstEntity.type === FOLDER) {
+      return -1;
+    }
+    return 1;
+  });
+  return sortedItems;
+}
 
 function mapStateToProps(state) {
   const {
@@ -18,7 +44,7 @@ function mapStateToProps(state) {
   } = state;
   return {
     view,
-    items,
+    items: sortItems(items, entities),
     entities,
     itemToUpdate: entities[itemToUpdateID],
     currentFolderID,
